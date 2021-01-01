f7ba231728482db96ea6dec31f7c37bac61a9790
Ariane Barbosa <aricbs@gmail.com>
2021-01-01 15:32:58 -0300

    Dependencias instaladas

--- financas/src/app/app.module.ts
+++ financas/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IMaskModule } from 'angular-imask';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    IMaskModule,
     AppRoutingModule
   ],
   providers: [],
