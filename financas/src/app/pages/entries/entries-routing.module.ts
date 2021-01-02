4008d7a7f27e4aed70157a47a099421d67610872
Ariane Barbosa <aricbs@gmail.com>
2021-01-02 00:49:23 -0300

    Adding EntryDetailComponent

--- financas/src/app/pages/entries/entries-routing.module.ts
+++ financas/src/app/pages/entries/entries-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EntryListComponent } from './entry-list/entry-list.component';
+import { EntryDetailComponent } from './entry-detail/entry-detail.component';
 
 const routes: Routes = [
   { path: '', component: EntryListComponent },
-  
+  { path: 'new', component: EntryDetailComponent },
+  { path: ':id/edit', component: EntryDetailComponent }
 ];
 
 @NgModule({
--- financas/src/app/pages/entries/entries.module.ts
+++ financas/src/app/pages/entries/entries.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 
 import { EntryListComponent } from './entry-list/entry-list.component';
+import { EntryDetailComponent } from './entry-detail/entry-detail.component';
 
 
 @NgModule({
   declarations: [
-    EntryListComponent
+    EntryListComponent,
+    EntryDetailComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     EntriesRoutingModule
   ]
 })
